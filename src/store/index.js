dbfa10b0dac2be62e583fd7ddc01593afe0aaee2
hedenica <hedenica@hotmail.com>
2020-06-11 21:45:35 -0300

    ✨  adding product to cart

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,10 @@ import { createStore } from 'redux';
 
 import rootReducer from './modules/rootReducer';
 
-const store = createStore(rootReducer);
+const enhacer = process.env.NODE_ENV === 'development'
+  ? console.tron.createEnhancer()
+  : null;
+
+const store = createStore(rootReducer, enhacer);
 
 export default store;
