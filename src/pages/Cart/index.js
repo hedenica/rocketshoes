1f19e17e7256c77efa3716f9919953701dc0cd9a
hedenica <hedenica@hotmail.com>
2020-06-11 22:22:29 -0300

    ✨  delete product from cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -5,7 +5,7 @@ import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons
 
 import { Container, ProductTable, Total } from './styles';
 
-function Cart({ cart }) {
+function Cart({ cart, dispatch }) {
   return (
     <Container>
       <ProductTable>
@@ -46,7 +46,7 @@ function Cart({ cart }) {
                 <strong>R$258,80</strong>
               </td>
               <td>
-                <button type="button">
+                <button type="button" onClick={() => dispatch({ type: 'REMOVE_FROM_CART', id: product.id })}>
                   <MdDelete size={20} color="#7159c1" />
                 </button>
               </td>
