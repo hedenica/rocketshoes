b86bb0e9ccc764e809ea21ec660b651b4ac8eeec
hedenica <hedenica@hotmail.com>
2020-06-15 20:19:34 -0300

    ✨  checking stock

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -12,9 +12,19 @@ function* addToCart({ id }) {
     (state) => state.cart.find((p) => p.id === id),
   );
 
-  if (productExist) {
-    const amount = productExist.amount + 1;
+  const stock = yield call(api.get, `/stock/${id}`);
 
+  const stockAmount = stock.data.amount;
+  const currentAmount = productExist ? productExist.amount : 0;
+
+  const amount = currentAmount + 1;
+
+  if (amount > stockAmount) {
+    console.tron.warn('ERRO');
+    return;
+  }
+
+  if (productExist) {
     yield put(updateAmount(id, amount));
   } else {
     const response = yield call(api.get, `/products/${id}`);
