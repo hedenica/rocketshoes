c84dd21c87755dccf47c936cbfe72e9f968050e4
hedenica <hedenica@hotmail.com>
2020-06-15 18:41:13 -0300

    ✨  adding reactotron plugin for saga

--- src/store/index.js
+++ src/store/index.js
@@ -4,14 +4,17 @@ import createSagaMiddleware from 'redux-saga';
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
 
-const sagaMiddleware = createSagaMiddleware();
+const sagaMonitor = process.env.NODE_ENV === 'development' ? console.tron.createSagaMonitor() : null;
+const sagaMiddleware = createSagaMiddleware({
+  sagaMonitor,
+});
 
-const enhacer = process.env.NODE_ENV === 'development'
+const enhancer = process.env.NODE_ENV === 'development'
   ? compose(console.tron.createEnhancer(),
     applyMiddleware(sagaMiddleware))
   : applyMiddleware(sagaMiddleware);
 
-const store = createStore(rootReducer, enhacer);
+const store = createStore(rootReducer, enhancer);
 
 sagaMiddleware.run(rootSaga);
 
